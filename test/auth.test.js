752c35ac16a5c97f36a0da3017dda3109216dee9
Andrii <Rudavko@gmail.com>
2019-03-14 18:01:37 +0200

    Remove unused mock

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,13 +1,7 @@
-const driveMock = {
-  files: {
-    list: jest.fn()
-  }
-}
 const google = {
   auth: {
     OAuth2: jest.fn()
-  },
-  drive: jest.fn(driveMock)
+  }
 }
 const { auth } = require('../src/auth')
 
