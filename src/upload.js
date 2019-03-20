e02206fc1a622d7555f621b60853b807b41ee70d
Andrii <Rudavko@gmail.com>
2019-03-20 13:46:04 +0200

    Change array to a Set in `upload`

--- src/upload.js
+++ src/upload.js
@@ -1,12 +1,12 @@
-const uploading = []
+const uploading = new Set([])
 exports.upload = ({ auth, config, files } = {}) => {
   if (!auth) return Promise.reject(new Error('The upload module did not receive the auth'))
   if (!config) return Promise.reject(new Error('The upload module did not receive the config'))
 
   if (files && files.length > 0) {
     files.forEach(file => {
-      if (uploading.length < (config.maxConcurent || 4)) {
-        uploading.push(file)
+      if (uploading.size < (config.maxConcurent || 4)) {
+        uploading.add(file)
         console.log(file, ' started uploading')
       } else {
         console.log(file, ' put in queue')
