17a4ebcac6f04ef18a885f17bfccd7f6ef823ff9
Andrii <Rudavko@gmail.com>
2019-03-29 18:54:15 +0200

    Update main.js
    Include moduls for injection, and change the structure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-exports.launch = ({ getConfig, getAuth, upload }) =>
-  getConfig()
+const fs = require('fs')
+const flatten = require('array-flatten')
+const { google } = require('googleapis')
+const { rl } = require('./modules/rl')
+const GoogleOAuth2 = google.auth.OAuth2
+
+exports.launch = ({ loadConfig, getAuth, upload, getFilesList }) =>
+  loadConfig()
     .then(config =>
-      getAuth(config)
+      getAuth({ config, GoogleOAuth2, rl })
         .then(auth =>
-          upload({ auth, config })))
+          getFilesList({ fs, config, flatten })
+            .then(files =>
+              upload({
+                fs,
+                auth,
+                files,
+                google,
+                config,
+                log: console.log }))))
+    .catch(er => console.log(er))
