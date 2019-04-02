5d0c8cd2c1add3187c25849e77dcf31452dad2da
Andrii <Rudavko@gmail.com>
2019-04-02 14:01:49 +0300

    Create tests for loadConfig

--- src/modules/loadConfig.js
+++ src/modules/loadConfig.js
@@ -1,18 +1,32 @@
-const { config } = require('../../config')
+const { config } = require('../../config.js')
 const fs = require('fs')
 const CREDENTIALS_PATH = './credentials.json'
+const TOKEN_PATH = './token.json'
 const SCOPE = 'https://www.googleapis.com/auth/drive.file'
-exports.loadConfig = () => {
+
+const loadConfig = ({ fs, config }) => {
   if (!fs.existsSync(CREDENTIALS_PATH)) {
     return Promise.reject(
       new Error('Credentials file "credentials.json" was not found at root of the project'))
   }
+
   const credentialsString = fs.readFileSync(CREDENTIALS_PATH)
   try {
-    config.credentials = JSON.parse(credentialsString).web
+    config.credentials = JSON.parse(credentialsString).installed
   } catch (e) {
     return Promise.reject(new Error('Could not parse "credentials.json" file, it was in-credible'))
+  }
+  if (fs.existsSync(TOKEN_PATH)) {
+    const tokenString = fs.readFileSync(TOKEN_PATH)
+    try {
+      config.token = JSON.parse(tokenString).refresh_token
+    } catch (e) {
+      return Promise.reject(new Error('Could not parse "token.json" file'))
+    }
   }
   config.scope = SCOPE
   return Promise.resolve(config)
 }
+
+exports.loadConfigClean = loadConfig
+exports.loadConfig = () => loadConfig({ fs, config })
---
+++ test/loadConfig.test.js
@@ -0,0 +1,86 @@
+const loadConfig = require('../src/modules/loadConfig').loadConfigClean
+const CREDENTIALS_PATH = './credentials.json'
+const TOKEN_PATH = './token.json'
+const SCOPE = 'https://www.googleapis.com/auth/drive.file'
+describe('loads config fine', () => {
+  const config = {}
+  it('returns error if credentials do not exist', done => {
+    const fs = {
+      existsSync: jest.fn()
+    }
+    loadConfig({ fs, config })
+      .catch(er => {
+        expect(fs.existsSync).toBeCalled()
+        expect(er).toEqual(new Error('Credentials file "credentials.json" was not found at root of the project'))
+        done()
+      })
+  })
+  it('returns error if creentials are unparsable', done => {
+    const fs = {
+      existsSync: jest.fn(() => true),
+      readFileSync: jest.fn()
+    }
+    loadConfig({ fs, config })
+      .catch(er => {
+        expect(fs.existsSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(er).toEqual(new Error('Could not parse "credentials.json" file, it was in-credible'))
+        done()
+      })
+  })
+  it('returns proper config object', done => {
+    const fs = {
+      existsSync: jest.fn(() => true),
+      readFileSync: jest.fn(() => '{}')
+    }
+    loadConfig({ fs, config })
+      .then(config => {
+        expect(fs.existsSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(fs.existsSync).toBeCalledWith(TOKEN_PATH)
+        expect(fs.readFileSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(config).toEqual({ credentials: undefined, scope: SCOPE })
+        done()
+      })
+  })
+  it('returns error on bad token file', done => {
+    const fs = {
+      existsSync: jest.fn(() => true),
+      readFileSync: jest.fn(path => {
+        if (path === TOKEN_PATH) {
+          return '{'
+        }
+        if (path === CREDENTIALS_PATH) {
+          return '{"installed":{"client_id":"ijijijji"}}'
+        }
+      })
+    }
+    loadConfig({ fs, config })
+      .catch(er => {
+        expect(fs.existsSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(fs.existsSync).toBeCalledWith(TOKEN_PATH)
+        expect(fs.readFileSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(er).toEqual(new Error('Could not parse "token.json" file'))
+        done()
+      })
+  })
+  it('returns proper config object', done => {
+    const fs = {
+      existsSync: jest.fn(() => true),
+      readFileSync: jest.fn(path => {
+        if (path === TOKEN_PATH) {
+          return '{"refresh_token":"ererererere"}'
+        }
+        if (path === CREDENTIALS_PATH) {
+          return '{"installed":{"client_id":"ijijijji"}}'
+        }
+      })
+    }
+    loadConfig({ fs, config })
+      .then(config => {
+        expect(fs.existsSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(fs.existsSync).toBeCalledWith(TOKEN_PATH)
+        expect(fs.readFileSync).toBeCalledWith(CREDENTIALS_PATH)
+        expect(config).toEqual({ credentials: { 'client_id': 'ijijijji' }, scope: SCOPE, token: 'ererererere' })
+        done()
+      })
+  })
+})
